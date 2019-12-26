4bd1412772b50148b46a9e8334241c2261dfa80c
Krishna Al Rasyid <krishnaalrasyid@gmail.com>
2019-12-26 11:26:22 +0700

    Working with 'Normal' Components

--- layouts/users.vue
+++ layouts/users.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
+    <Header />
     <header>
       USERS SECTION
     </header>
     <nuxt />
   </div>
 </template>
+
+<script>
+import Header from '@/components/header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
 
 <style>
 
